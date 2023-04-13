ede1704a6e05a1cfe153c1a7d8a9914f9500bd21
swj2023 <qkedkfkvkd@gmail.com>
2023-04-13 10:57:14 +0900

    StudentAttendanceActivity  - 학생 권한 등원 / 하원 처리 완료

--- app/src/main/java/com/swj/academymanagement/G.kt
+++ app/src/main/java/com/swj/academymanagement/G.kt
@@ -6,5 +6,8 @@ class G {
     companion object {
         // 전체 화면에서 사용할 멤버 객체 ( 선생님, 학생 )
         var member:Member = Member("", "", "", "", "", call_number = "")
+
+        // 학생 권한 등원 완료 시 attendance 테이블의 attendanceCode (PK) 가져오기
+        var attendanceCode:String = ""
     }
 }
--- app/src/main/java/com/swj/academymanagement/activities/StudentAttendanceActivity.kt
+++ app/src/main/java/com/swj/academymanagement/activities/StudentAttendanceActivity.kt
@@ -1,12 +1,20 @@
 package com.swj.academymanagement.activities
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.swj.academymanagement.G
 import com.swj.academymanagement.databinding.ActivityStudentAttendanceBinding
+import com.swj.academymanagement.network.RetrofitAttendanceService
+import com.swj.academymanagement.network.RetrofitHelper
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.TimeZone
@@ -44,22 +52,94 @@ class StudentAttendanceActivity : AppCompatActivity() {
         // 등원 시간
         var attendanceTime = ""
         // 하원 시간
-        var gohomeTime = ""
+        var theLowerHouseTime = ""
 
         // 등원 버튼
         binding.btnAttendance.setOnClickListener {
+            // 등원 버튼 눌렀을 당시의 시간 가져오기
             sdf = SimpleDateFormat("HH:mm")
             sdf.timeZone = TimeZone.getTimeZone("Asia/Seoul")
+
+            // 등원 시간에 가져온 시간 대입
             attendanceTime = sdf.format(Date())
-            Toast.makeText(this, attendanceTime, Toast.LENGTH_SHORT).show()
+
+            // retrofit attendance Table Insert 작업
+            RetrofitHelper.getRetrofitInstance().create(RetrofitAttendanceService::class.java)
+                .studentAttendance(
+                    G.member.id,        // 학생 아이디
+                    attendanceTime      // 등원 시간
+                ).enqueue(object :Callback<String> {
+                    override fun onResponse(call: Call<String>, response: Response<String>) {
+                        val message = response.body()
+
+                        if(message != null) {
+                            // attendance 테이블에 insert 처리된 경우 넘어오는 문자열이 [attendanceCode],[학생 이름] 등원 완료! 이므로..
+                            val messageArr = message.split(",")
+
+                            // attendance 테이블에 insert 처리된 경우 넘어오는 문자열이 [attendanceCode],[학생 이름] 등원 완료! 이므로.. size는 2가 된다.
+                            if(messageArr.size > 1) {
+                                // attendance 테이블에 Insert한 후 하원 버튼 클릭시 attendance의 PK인 attendanceCode가 필요하므로..
+                                // 글로벌 전역 변수 클래스인 G 클래스의 attendanceCode에 넣어준다.
+                                G.attendanceCode = messageArr[0]
+
+                                Toast.makeText(this@StudentAttendanceActivity, messageArr[1], Toast.LENGTH_SHORT).show()
+
+                                if(messageArr[1].contains("완료")) {  // [학생 이름] 등원 완료! 이므로..
+                                    startActivity(Intent(this@StudentAttendanceActivity, StudentActivity::class.java))
+                                    finish()
+                                }
+                            } else {
+                                // 이미 attendance 테이블에 insert 처리된 경우 넘어오는 문자열이 이미 등원 처리되었습니다. 이므로.. size는 1이 된다.
+                                Toast.makeText(this@StudentAttendanceActivity, messageArr[0], Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this@StudentAttendanceActivity, StudentActivity::class.java))
+                                finish()
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<String>, t: Throwable) {
+                        AlertDialog.Builder(this@StudentAttendanceActivity)
+                            .setMessage("error : ${t.message}")
+                            .setPositiveButton("OK", null).show()
+                    }
+                })
         }
 
         // 하원 버튼
         binding.btnGohome.setOnClickListener {
+            // 하원 버튼 눌렀을 당시의 시간 가져오기
             sdf = SimpleDateFormat("HH:mm")
             sdf.timeZone = TimeZone.getTimeZone("Asia/Seoul")
-            gohomeTime = sdf.format(Date())
-            Toast.makeText(this, gohomeTime, Toast.LENGTH_SHORT).show()
+
+            // 하원 시간에 가져온 시간 대입
+            theLowerHouseTime = sdf.format(Date())
+
+            // retrofit attendance Table Update 작업
+            RetrofitHelper.getRetrofitInstance().create(RetrofitAttendanceService::class.java)
+                .studentTheLowerHouse(
+                    G.attendanceCode,       // attendance 테이블의 attendanceCode (PK)
+                    G.member.id,            // 학생 아이디
+                    theLowerHouseTime       // 하원 시간
+                ).enqueue(object :Callback<String> {
+                    override fun onResponse(call: Call<String>, response: Response<String>) {
+                        val message = response.body()
+
+                        if(message != null) {
+                            Toast.makeText(this@StudentAttendanceActivity, message, Toast.LENGTH_SHORT).show()
+
+                            if(message.contains("완료")) {  // [학생 이름] 하원 완료! 이므로..
+                                startActivity(Intent(this@StudentAttendanceActivity, StudentActivity::class.java))
+                                finish()
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<String>, t: Throwable) {
+                        AlertDialog.Builder(this@StudentAttendanceActivity)
+                            .setMessage("error : ${t.message}")
+                            .setPositiveButton("OK", null).show()
+                    }
+                })
         }
     }
 }
